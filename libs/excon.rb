17fe82d2a922df291fc89596afe4a68973fe2917
Chris Heald <cheald@gmail.com>
2014-02-18 13:59:03 -0700

    Fix Excon client creation and usage

--- libs/excon.rb
+++ libs/excon.rb
@@ -1,22 +1,25 @@
 require 'excon'
 class ExconBenchmark < BaseBenchmark
   COLOR = "#BB8B00"
-  def setup
+  def setup_sync
     @client = nil
-    Excon.defaults[:middlewares] << Excon::Middleware::Decompress unless Excon.defaults[:middlewares].include? Excon::Middleware::Decompress
   end
 
   def run_sync(url)
-    @client ||= Excon.new(url, :persistent => true)
-    @client.get(url).body
+    @client ||= Excon.new(url, persistent: true)
+    @client.get.body
   end
 
   ####################################
 
+  def setup_gzip
+    @client = nil
+  end
+
   def run_gzip(*urls)
     urls.each do |url|
-      @client ||= Excon.new(url, :persistent => true)
-      @client.get(url, headers: {"accept-encoding" => "gzip, deflate"}).body
+      @client ||= Excon.new(url, :persistent => true, middlewares: Excon.defaults[:middlewares] + Array(Excon::Middleware::Decompress))
+      @client.get.body
     end
   end
 end
